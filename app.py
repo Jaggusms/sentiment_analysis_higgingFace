72a8cbb6699cc56b23bf37936b6e9358b89015e5
Jagadeesh <38516306+Jaggusms@users.noreply.github.com>
2023-07-15 19:54:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 import streamlit as st
-from transformers import BertTokenizer, BertForSequenceClassification
-import torch
+from transformers import pipeline
  # 👈 Add the caching decorator
 @st.cache(allow_output_mutation=True)
-def get_model():
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    model = BertForSequenceClassification.from_pretrained("ProsusAI/finbert")
-    return tokenizer,model
-#model = load_model()
+def load_model():
+    return pipeline("sentiment-analysis")
+
+model = load_model()
 
 query = st.text_input("Your query")
 if query:
-    #result = model(query)[0]  # 👈 Classify the query text
-    #st.write(result["label"])
-    st.write("hi")
+    result = model(query)[0]  # 👈 Classify the query text
+    st.write(result["label"])
